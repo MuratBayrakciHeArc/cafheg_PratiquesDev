71ff7691c93c668432fef1d4f352c1757652b118
MuratBayrakciHeArc <murat.bayrakci@he-arc.ch>
2025-05-29 01:41:56 +0200

    Ex5 p2 done

---
+++ src/integration-test/java/MyTestsIT.java
@@ -0,0 +1,17 @@
+
+
+
+
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class MyTestsIT {
+
+    @Test
+    void simpleTest() {
+        assertThat(1).isEqualTo(1);
+    }
+}
+
